e6c0f23aa29b1dea7f0e665113085260a544fc5d
Samuel Villasenor <alen.samuelvf@gmail.com>
2021-03-21 12:40:45 -0700

    updating redirect the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux'
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign.up.component';
@@ -44,16 +46,20 @@ class App extends React.Component {
     <Header />
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
-      <Route exact path='/signin' component={SignInAndSignUpPage} />
+      <Route path='/shop' component={ShopPage} />
+      <Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
     </Switch>
   </div> 
   );
 }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
